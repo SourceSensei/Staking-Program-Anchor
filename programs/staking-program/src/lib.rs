7e18f2969fd27f63008bbd1e68152ea7288baa9f
SourceSensei <source.sensei1205@gmail.com>
2023-10-17 00:25:35 +0100

    second update

--- programs/staking-program/src/lib.rs
+++ programs/staking-program/src/lib.rs
@@ -2,6 +2,12 @@ use anchor_lang::prelude::*;
 
 declare_id!("YDmDZLkm7q8L3cBNRuAYqL4XvLQ2FQPHgLE5MzJQwhb");
 
+pub mod constants {
+    pub const VAULT_SEED: &[u8] = b"vault";
+    pub const STAKE_INFO_SEED: &[u8] = b"stake_info";
+    pub const TOKEN_SEED: &[u8] = b"token";
+}
+
 #[program]
 pub mod staking_program {
     use super::*;
@@ -9,7 +15,16 @@ pub mod staking_program {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
+
+    pub fn stake(ctx: Context<Initialize>, amount: u64) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn destake(ctx: Context<Initialize>, amount: u64) -> Result<()> {
+        Ok(())
+    }
 }
+
 
 #[derive(Accounts)]
 pub struct Initialize {}
